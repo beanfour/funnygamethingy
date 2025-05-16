27453578966a96496950f024cdd32e90ef6b8c0c
beanfour <contactskylarluong@gmail.com>
2025-05-16 13:40:38 -0700

    button code edit

--- Playbutton.cs
+++ Playbutton.cs
@@ -3,4 +3,13 @@ using System;
 
 public partial class Playbutton : Button
 {
+	public override void _Ready()
+	{
+		Pressed += OnPlayButtonPressed;
+	}
+
+	private void OnPlayButtonPressed()
+	{
+		GD.Print("Play button pressed!");
+	}
 }
