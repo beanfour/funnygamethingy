0221e79004c4bf9dffd9b8dd614edd42067d7b7e
beanfour <contactskylarluong@gmail.com>
2025-05-16 15:36:24 -0700

    SD

---
+++ buttontest.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class buttontest : Button
+{
+	public override void _Ready()
+	{
+		var button = new Button();
+		button.Text = "PRESS MEEE!!!";
+		button.Pressed += ButtonPressed;
+		button.Size = new Vector2(150, 200); // Set button size to 150x200
+		AddChild(button);
+	}
+
+	private void ButtonPressed()
+	{
+		GD.Print("Hello world!");
+	}
+}
