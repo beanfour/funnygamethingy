6d0e216fbbac814e089ba4aa493e3f01199eb0c6
beanfour <contactskylarluong@gmail.com>
2025-05-20 23:33:34 -0700

    added walking

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,11 +3,17 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	// Movement speed in pixels per second
 	[Export]
 	public int Speed = 200;
 
-	public void movement(double delta)
+	private AnimatedSprite2D _anim;
+
+	public override void _Ready()
+	{
+		_anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
+	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Vector2.Zero;
 
@@ -16,12 +22,28 @@ public partial class Player : CharacterBody2D
 		if (Input.IsActionPressed("ui_left"))
 			velocity.X -= 1;
 		if (Input.IsActionPressed("ui_down"))
-			velocity.Y += 1;
-		if (Input.IsActionPressed("ui_up"))
+			velocity.Y += 1;d))
 			velocity.Y -= 1;
 
+		// Normalize for diagonal movement
 		velocity = velocity.Normalized() * Speed;
 		Velocity = velocity;
 		MoveAndSlide();
+
+		// Play animation based on movement
+		if (velocity.Length() > 0)
+		{
+			_anim.Play("idle");
+		}
+		else
+		{
+			_anim.Play("walk_side");
+		}
+
+		// Flip sprite based on direction (optional)
+		if (velocity.X != 0)
+		{
+			_anim.FlipH = velocity.X < 0;
+		}
 	}
 }
